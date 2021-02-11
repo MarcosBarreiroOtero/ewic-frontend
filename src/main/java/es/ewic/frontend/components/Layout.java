7893cc11b75f17832fa4de5c89fb5ec72e9673fb
Marcos <marcos.barreiro.otero@udc.es>
2021-02-11 10:28:57 +0100

    Solucionado layout

--- src/main/java/es/ewic/frontend/components/Layout.java
+++ src/main/java/es/ewic/frontend/components/Layout.java
@@ -1,11 +1,16 @@
 package es.ewic.frontend.components;
 
+import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SessionState;
 import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
 
+import es.ewic.frontend.pages.Index;
+import es.ewic.frontend.services.AuthenticationPolicy;
+import es.ewic.frontend.services.AuthenticationPolicyType;
 import es.ewic.frontend.util.UserSession;
 
 /**
@@ -19,9 +24,22 @@ public class Layout {
 
 	@Inject
 	Messages messages;
+	@Inject
+	private Request request;
 
 	@Property
 	@SessionState(create = false)
-	private UserSession UserSession;
+	private UserSession userSession;
+
+	@InjectPage
+	private Index index;
+
+	@AuthenticationPolicy(AuthenticationPolicyType.AUTHENTICATED_USER)
+	Object onActionFromLogOut() {
+		userSession = null;
+		request.getSession(false).invalidate();
+		return index;
+
+	}
 
 }
